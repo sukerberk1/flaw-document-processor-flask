62ea674c8315880b49b8e02aa04d6bf9cbf39f8f
Chris Kinsman <chrisknsmn@gmail.com>
2025-03-27 18:08:47 -0400

    Initial commit

--- src/PdfSummary/Features/PdfProcessing/IPdfSummarizer.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace PdfSummary.Features.PdfProcessing
-{
-    public interface IPdfSummarizer
-    {
-        Task<string> SummarizeTextAsync(string text);
-    }
-}
