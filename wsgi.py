d745ab74aae43266d30f5aa181be07cdd20ed21b
Chris Kinsman <chrisknsmn@gmail.com>
2025-03-29 12:51:40 -0400

    render fix

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the project root directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from app import create_app
 
 app = create_app()
